4bc7796eae4024bafbbe25557f73c8e330550988
trolek12321 <aleks.oldakowski@wp.pl>
2024-12-03 11:42:30 +0100

    SkeletonRange
    - SkeletonRange movement
    - SkeletonRange attack

---
+++ Assets/scripts/enemy/Arrow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace enemy
+{
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private float damage = 2f;
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            player.health -= damage;
+            Debug.Log("Strzała trafiła gracza!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+}
