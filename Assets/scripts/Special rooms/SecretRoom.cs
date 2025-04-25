e4264af17ed47d5d8052673bba023c7682bb7e4e
PiotrCodi <151371404+PiotrCDV@users.noreply.github.com>
2025-04-26 00:11:41 +0200

    Secret room

---
+++ Assets/scripts/Special rooms/SecretRoom.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretRoom : MonoBehaviour
+{
+    [SerializeField] private ItemSpawner[] itemSpawners;
+
+    [SerializeField] private GameObject returnPortalPrefab;
+    [SerializeField] private Vector3 portalSpawnPosition;
+
+
+    public void DestroyItem(GameObject collectedItem)
+    {
+
+
+        foreach (ItemSpawner itemSpawner in itemSpawners)
+        {
+            if (itemSpawner != null && itemSpawner.gameObject != collectedItem)
+            {
+                Destroy(itemSpawner.gameObject);
+            }
+        }
+
+        CreateReturnPortal();
+
+    }
+
+    void CreateReturnPortal()
+    {
+        Instantiate(returnPortalPrefab, portalSpawnPosition, Quaternion.identity);
+    }
+}
