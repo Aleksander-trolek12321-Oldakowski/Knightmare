d770315745f951aa2ad3752ec499363f6e023d8b
PiotrCodi <151371404+PiotrCDV@users.noreply.github.com>
2025-05-26 18:01:54 +0200

    Small changes

--- Assets/scripts/enemy/BossWalkingSound.cs
+++ Assets/scripts/enemy/BossWalkingSound.cs
@@ -5,22 +5,53 @@ using UnityEngine;
 public class BossWalkingSound : MonoBehaviour
 {
     public LayerMask PlayerMask;
+    public Rigidbody2D bossRigidbody;
+    public float movementThreshold = 0.1f;
+
+    private bool isPlayerInside = false;
+    private bool isSoundPlaying = false;
+
+    private void Update()
+    {
+        if (isPlayerInside)
+        {
+            if (bossRigidbody != null && bossRigidbody.velocity.magnitude > movementThreshold)
+            {
+                if (!isSoundPlaying)
+                {
+                    AudioManager.Instance.PlaySound("BossWalkingSound");
+                    isSoundPlaying = true;
+                }
+            }
+            else
+            {
+                if (isSoundPlaying)
+                {
+                    AudioManager.Instance.StopSound("BossWalkingSound");
+                    isSoundPlaying = false;
+                }
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-
         if (((1 << other.gameObject.layer) & PlayerMask) != 0)
         {
-            AudioManager.Instance.PlaySound("BossWalkingSound");
-       
+            isPlayerInside = true;
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (((1 << other.gameObject.layer) & PlayerMask) != 0)
         {
-            AudioManager.Instance.StopSound("BossWalkingSound");
+            isPlayerInside = false;
+            if (isSoundPlaying)
+            {
+                AudioManager.Instance.StopSound("BossWalkingSound");
+                isSoundPlaying = false;
+            }
         }
-
     }
 }
