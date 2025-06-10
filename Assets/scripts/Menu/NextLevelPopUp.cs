25f0996b3f02e4221ad0c3a264fc35d9cba7eddb
PiotrCodi <151371404+PiotrCDV@users.noreply.github.com>
2025-06-10 15:50:18 +0200

    Next Level UI

---
+++ Assets/scripts/Menu/NextLevelPopUp.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class NextLevelPopUp : MonoBehaviour
+{
+    [SerializeField] private GameObject nextLevelPortal;
+
+  
+    public void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+    }
+    public void Yes()
+    {
+        SceneManager.LoadScene("level 2");
+
+    }
+    public void No()
+    {
+        nextLevelPortal.SetActive(false);
+
+    }
+
+    public void Menu()
+    {
+        AudioManager.Instance.StopSound("BossMusicAfterKill");
+
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+
+}
